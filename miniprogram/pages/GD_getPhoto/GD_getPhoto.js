a0e745847cc576aabf31df589ea94630a79d0641
cuishuting <1713083@mail.nankai.edu.cn>
2020-05-25 18:11:46 +0800

    add the skin-check api

--- miniprogram/pages/GD_getPhoto/GD_getPhoto.js
+++ miniprogram/pages/GD_getPhoto/GD_getPhoto.js
@@ -7,6 +7,8 @@ Page({
   data: {
     imagePath:"", //初始化时一定要加“”（类型需要）
     takePhotoOrNot:true,
+    gettingPhoto: true,
+    base64_photo: "",
   },
 
   /**
@@ -15,35 +17,46 @@ Page({
   onLoad: function (options) {
 
   },
-  takePhoto: function() {
-    this.setData({
-      takePhotoOrNot: false,
-    });
-    var ctx = wx.createCameraContext();
-    ctx.takePhoto({
-      quality: 'high',
-      success: (res) => {
-        this.setData({
-          imagePath: res.tempImagePath
-        })
-      },
-      error(e) {
-        console.log(e.detail);
-      }
-    })
-  },
+  //接下来需要完善请求api接口的调用
   upLoadImage: function() {
+    var that = this;
     wx.chooseImage({
       count:1,
       sizeType:['original', 'compressed'],
-      sourceType: ['album'],
+      sourceType: ['album', 'camera'],
       success: (res) => {
-        this.setData({
+        that.setData({
           imagePath: res.tempFilePaths,
         });
+        var curPhotoSize = res.tempFiles[0].size;
+        if (curPhotoSize <= 2000000) {
+          var base64 = wx.getFileSystemManager().readFileSync(res.tempFilePaths[0], 'base64');
+          wx.uploadFile({
+            filePath: res.tempFilePaths[0],
+            name: 'file',
+            url: 'https://api-facestyle.megvii.com/facestyle/v1/skinanalyze',
+            fail: (res) => {
+              console.log("调用失败");
+            },
+            header: {
+              'content-type' : 'multipart/form-data'
+            },
+            formData: {
+              'api_key' : 's4OuYgKYS9p1gmk0VpAu5RkPPs6So9gb',
+              'api_secret' : '88bk_yKW4lheqB2ieV6UWS_oNVwxBcaT',
+              'image_base64' : base64
+            },
+            success: (result) => {
+              console.log(result.data);
+            },
+          })
+        }
+        else {
+          console.log("图片尺寸大于2m");
+        }
         
       }
-    })
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
