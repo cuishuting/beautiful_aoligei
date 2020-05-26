24522579f9ac38cc7e20452665ee8073bf644cb2
cuishuting <1713083@mail.nankai.edu.cn>
2020-05-26 14:46:32 +0800

    add modification of recommend page

--- miniprogram/pages/GD_recommend/GD_recommend.js
+++ miniprogram/pages/GD_recommend/GD_recommend.js
@@ -5,9 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    TabCur: 0,
+    list:[],
+    list0: [
+      {
+        name: "DIOR花秘瑰萃洁颜泡沫",
+        picture:"https://www.dior.cn/beauty/version-5.1563986503613/resize-image/ep/3000/2000/90/0/horizon%252Fcovers%252FY0996035_C099600035_E01_ZHC.jpg",
+        cost: "2000"
+      },
+      {
+        name: "DIOR肌活蕴能洁面凝露",
+        picture:"https://www.dior.cn/beauty/version-5.1563986503613/resize-image/ep/3000/2000/90/0/%252FY0996419%252FV002%252FY0996419_C099600419_E02_ZHC.jpg",
+        cost: "1800"
+      }
+    ],
+    list1: [
+      {
+        name: "DIOR乐肤源澎澎弹盈润面膜",
+        picture:"https://www.dior.cn/beauty/version-5.1563986503613/resize-image/ep/3000/2000/90/0/horizon%252Fcovers%252FY0691530_F069153080_E01_ZHC.jpg",
+        cost: "1000"
+      }
+    ],
+    list2:[],
+    list3:[]
+  }, 
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+    })
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -63,4 +89,4 @@ Page({
   onShareAppMessage: function () {
 
   }
-})
+}) 
